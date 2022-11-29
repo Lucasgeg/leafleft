81a5efc17c650ea4d0262f57de248b524c61e549
Lucasgeg <99218923+Lucasgeg@users.noreply.github.com>
2022-11-29 07:40:53 +0100

    changer api geoloc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { TileLayer, MapContainer } from "react-leaflet";
+import { Marker, Popup } from "react-leaflet";
+import dataset from "./data/restaurants-references.json";
+import SearchBar from "./SearchBar";
+import { useState } from "react";
 
 function App() {
+  const [placeFocus, setPlaceFocus] = useState([48.485588, -4.252345]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="">
+        <SearchBar {...setPlaceFocus} />
+      </div>
+      <MapContainer center={placeFocus} zoom={17}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          // url={`https://tile.jawg.io/jawg-streets/{z}/{x}/{y}.png?lang=fr&access-token=${token}`}
+          attribution='&copy; <a href="http://www.osm.org/copyright">OpenStreetMap</a>'
+        />
+        {dataset.map((restaurant) => {
+          return (
+            <Marker
+              key={restaurant.recordid}
+              position={[
+                restaurant.geometry.coordinates[1],
+                restaurant.geometry.coordinates[0],
+              ]}
+            >
+              <Popup>
+                <h2>{restaurant.fields.nom_de_l_offre}</h2>
+                <p>{restaurant.fields.adresse}</p>
+              </Popup>
+            </Marker>
+          );
+        })}
+      </MapContainer>
+    </>
   );
 }
 
---
+++ src/SearchBar.js
@@ -0,0 +1,62 @@
+import { MenuItem, Select } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import axios from "axios";
+import { useState } from "react";
+
+const SearchBar = ({ setPlaceFocus }) => {
+  const [inputSearch, setInputSearch] = useState("");
+  const API_ADRESS_URL = `https://api-adresse.data.gouv.fr/search/?q=${inputSearch}`;
+  const [searchResult, setSearchResult] = useState([]);
+  const [searchBool, setSearchBool] = useState(false);
+  const startSearch = async () => {
+    await axios
+      .get(API_ADRESS_URL)
+      .then((res) => {
+        console.log(res);
+        setSearchResult(res.data.features);
+      })
+      .catch((err) => console.log(err));
+  };
+  const [resultSelect, setResultSelect] = useState({
+    label: "",
+    lat: undefined,
+    lng: undefined,
+  });
+  return (
+    <div className="searchBox">
+      <TextField
+        id="outlined-basic"
+        label="Barre de recherche"
+        variant="outlined"
+        onChange={(e) => setInputSearch(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            setSearchBool(true);
+            return startSearch();
+          }
+        }}
+      />
+      <Select value={""} open={searchBool}>
+        {searchResult.map((result) => {
+          return (
+            <MenuItem
+              key={result.properties.id}
+              onClick={() => {
+                setResultSelect({
+                  label: result.properties.label,
+                  lat: result.geometry.coordinates[1],
+                  lng: result.geometry.coordinates[0],
+                });
+                setPlaceFocus([resultSelect.lat, resultSelect.lng]);
+              }}
+            >
+              {result.properties.label}
+            </MenuItem>
+          );
+        })}
+      </Select>
+    </div>
+  );
+};
+
+export default SearchBar;
